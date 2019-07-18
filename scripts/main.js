922bc6405e8fbb8f9e67a25dc97eb3880446eda9
Jacquelyn M <jacquelynkmccray@gmail.com>
2019-07-18 14:43:24 -0500

    completed challenge on dom fragments

--- scripts/main.js
+++ scripts/main.js
@@ -8,9 +8,31 @@ listItem.textContent = "George Bush"
 
 list.appendChild(listItem)
 
-console.log(list)
+// console.log(list)
 
 // Challenge: 
+// const messages = document.querySelector("#messages")
+// const newMessage1 = document.createElement("section")
+// newMessage1.textContent = "Monday's lunch: Tuna sandwich"
+// const newMessage2 = document.createElement("section")
+// newMessage2.textContent = "Tuesday's lunch: Turkey sandwich"
+// const newMessage3 = document.createElement("section")
+// newMessage3.textContent = "Wednesday's lunch: Go out to eat"
+// const newMessage4 = document.createElement("section")
+// newMessage4.textContent = "Thusday's lunch: Salad"
+// const newMessage5 = document.createElement("section")
+// newMessage5.textContent = "Friday's lunch: Fried chicken"
+
+// messages.appendChild(newMessage1)
+// messages.appendChild(newMessage2)
+// messages.appendChild(newMessage3)
+// messages.appendChild(newMessage4)
+// messages.appendChild(newMessage5)
+
+
+// ------ Advanced Challenge: DOM Fragments ------
+const fragment = document.createDocumentFragment()
+
 const messages = document.querySelector("#messages")
 const newMessage1 = document.createElement("section")
 newMessage1.textContent = "Monday's lunch: Tuna sandwich"
@@ -23,8 +45,10 @@ newMessage4.textContent = "Thusday's lunch: Salad"
 const newMessage5 = document.createElement("section")
 newMessage5.textContent = "Friday's lunch: Fried chicken"
 
-messages.appendChild(newMessage1)
-messages.appendChild(newMessage2)
-messages.appendChild(newMessage3)
-messages.appendChild(newMessage4)
-messages.appendChild(newMessage5)
+fragment.appendChild(newMessage1)
+fragment.appendChild(newMessage2)
+fragment.appendChild(newMessage3)
+fragment.appendChild(newMessage4)
+fragment.appendChild(newMessage5)
+
+messages.appendChild(fragment)
